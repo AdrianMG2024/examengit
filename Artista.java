cc7b56811611c6b3a288722a4ec628772938f7cf
AdrianMG2024 <adrian.moya.gutierrez.alu@iesfernandoaguilar.es>
2024-03-18 13:57:21 +0100

    commit artista 2

---
+++ Artista.java
@@ -0,0 +1,31 @@
+package evf.javadoc.original;
+
+public class Artista {
+    String nombreArtista;
+	//
+    public Artista(String nombreArtista) {
+	//Hola
+        this.nombreArtista = nombreArtista;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "nombreArtista='" + nombreArtista + '\'' +
+                '}';
+    }
+
+    public String getNombreArtista() {
+        return nombreArtista;
+	//Esto es un comentario
+    }
+	
+    public void setNombreArtista(String nombre) {
+        this.nombreArtista = nombre;
+        //Adrian Moya
+	//Nuevo comentario ramas
+    }
+    
+}
+
+
